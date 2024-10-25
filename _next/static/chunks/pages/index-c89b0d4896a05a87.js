98f4c4bfe9dab4ad07885098242acf71bcdbb87d
rainmannerr <malisukarri@gmail.com>
2024-10-25 10:18:16 +0800

    Add files via upload

---
+++ _next/static/chunks/pages/index-c89b0d4896a05a87.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95405],{48312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(47925)}])},47925:function(n,u,_){"use strict";_.r(u);var t=_(42317);u.default=t.default}},function(n){n.O(0,[60296,69260,7368,16733,49774,92888,40179],function(){return n(n.s=48312)}),_N_E=n.O()}]);
